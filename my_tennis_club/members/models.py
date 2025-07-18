84560bf6ac15ffa4e6ba1a8cbd513c559beb87eb
Camilo Avila <camitlopa205@gmail.com>
2025-07-18 12:15:25 -0500

    Hicimos el método CRUD-DJANGO con el panel administrativo de los usuarios members

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -6,3 +6,6 @@ class Member(models.Model):
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
     email = models.EmailField(max_length=254, null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.id} {self.firstname} {self.lastname}"
