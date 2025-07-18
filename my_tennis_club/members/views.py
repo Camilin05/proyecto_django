9f67870bcd531c3c5553595897ea909037f0195f
Camilo Avila <camitlopa205@gmail.com>
2025-07-18 10:54:07 -0500

    Se agregó un testing y agregamos una plantilla y se modificó el estilo

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -20,3 +20,9 @@ def details(request, id):
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+def testing(request):
+    template = loader.get_template('template.html')
+    context = {
+    'aprendices': ['Camilo', 'Harold', 'Julian'],   
+    }
+    return HttpResponse(template.render(context, request))
